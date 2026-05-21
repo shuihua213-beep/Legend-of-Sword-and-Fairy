7f111c5ebecbb751e50ace93be001d5b4fab4847
test <test@example.com>
2026-05-21 06:02:41 +0000

    2.2

--- src/cn/tedu/xjqxz/GameFrame.java
+++ src/cn/tedu/xjqxz/GameFrame.java
@@ -1,54 +1,38 @@
 package cn.tedu.xjqxz;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-/**
- * 游戏案例的窗口类
- * 窗口大小：1024 * 768
- *
- * @author fgksgf
- */
 public class GameFrame {
     public static void main(String[] args) {
-        /*
-         * 1.创建窗口类的实例化对象
-         * 2.让窗口对象显示出来
-         * 3.设置窗口对象的相关属性
-         */
+        EventQueue.invokeLater(() -> {
+            JFrame jFrame = new JFrame();
+            jFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            jFrame.setIgnoreRepaint(true);
+            jFrame.setResizable(false);
+            jFrame.setTitle("仙剑奇侠传 - Version 1.0");
 
-        final int width = 1024;
-        final int height = 768;
-
-        JFrame jFrame = new JFrame();
-        jFrame.setSize(width, height);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setResizable(false);
-        jFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-        jFrame.addWindowListener(new WindowAdapter() {
-            // 当窗口正要关闭的时候，会自动调用该方法
-            @Override
-            public void windowClosing(WindowEvent e) {
-                super.windowClosing(e);
-                // 弹出确认对话框
-                int ch = JOptionPane.showConfirmDialog(null,"确认退出游戏吗？","提示",JOptionPane.YES_NO_OPTION);
-                if (ch == JOptionPane.YES_OPTION) {
-                    System.exit(0);
+            GamePanel gamePanel = new GamePanel();
+            jFrame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    super.windowClosing(e);
+                    int ch = JOptionPane.showConfirmDialog(null, "确认退出游戏吗？", "提示", JOptionPane.YES_NO_OPTION);
+                    if (ch == JOptionPane.YES_OPTION) {
+                        System.exit(0);
+                    }
+                    EventQueue.invokeLater(gamePanel::requestFocusInWindow);
                 }
-            }
+            });
+
+            jFrame.addKeyListener(gamePanel);
+            jFrame.add(gamePanel);
+            jFrame.pack();
+            jFrame.setLocationRelativeTo(null);
+            jFrame.setVisible(true);
+            gamePanel.requestFocusInWindow();
         });
-
-
-        jFrame.setTitle("仙剑奇侠传 - Version 1.0");
-
-
-        GamePanel gamePanel = new GamePanel();
-        jFrame.addKeyListener(gamePanel);
-        jFrame.add(gamePanel);
-
-        jFrame.setVisible(true);
-
     }
 }
